78e0c3babd8a665c15d9181f29cfd73b23486b5b
jgumerove <josh.gumerove@gmail.com>
2022-02-03 18:30:11 -0500

    use strict

---
+++ use_strict.js
@@ -0,0 +1,21 @@
+"use strict"
+let height = 70
+function weird(){
+  height = 50
+  return height
+}
+
+console.log(weird())
+
+// console.log("1-----")
+// console.log(teddy) //note --> we need to declare the variable first 
+// console.log(sing())
+// function sing() {
+//   console.log("ooh llala ala la")
+// }
+// var teddy = "bear"
+// let josh = "josh"
+// function sayHi(){
+//   return "hi"
+// }
+// console.log(this)
