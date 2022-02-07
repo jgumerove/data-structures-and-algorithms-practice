8adcc82401b0d7338032c9a3e049fba4dea86856
jgumerove <josh.gumerove@gmail.com>
2022-02-07 14:39:39 -0500

    closure practice

---
+++ closures.js
@@ -0,0 +1,41 @@
+// function callMeMaybe() {
+//     //const callMe = "hi i am now here"
+//     setTimeout(function() {
+//       console.log(callMe)
+//     }, 4000)
+//     const callMe = "testing if works"
+//   }
+
+  //callMeMaybe()
+
+  //note the above still works --> because we have already assigned callMe variable when function runs 
+
+  function test(){
+      let other = "josh"
+      return function a(){
+          return other
+      }
+  }
+
+  //the above demonstration only works with certain callbacks
+
+  //console.log(test()())
+
+  const makeNuclearButton = () => {
+    let timeWithoutDestruction = 0
+    const passTime = () => timeWithoutDestruction++
+    const totalPeaceTime = () => timeWithoutDestruction
+    const launch = () => {
+        timeWithoutDestruction = -1
+        return "boom"
+    }
+    setInterval(passTime, 1000)
+    return {
+        totalPeaceTime: totalPeaceTime,
+        passTime
+    }
+  }
+
+  const ohno = makeNuclearButton()
+  console.log(ohno.totalPeaceTime(), ohno.totalPeaceTime())
+  console.log(ohno.passTime())
